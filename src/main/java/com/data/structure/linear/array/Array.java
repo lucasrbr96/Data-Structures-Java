d8157d2376f426ac53ca504e95dfbf32bc7595e5
Lucas Ramos <lucasramosgab90@gmail.com>
2024-02-27 19:59:16 -0300

    Array - Data Structure

--- src/main/java/com/data/structure/linear/array/Array.java
+++ src/main/java/com/data/structure/linear/array/Array.java
@@ -1,2 +1,104 @@
-package com.data.structure.linear.array;public class Array {
+package com.data.structure.linear.array;
+
+import java.util.Arrays;
+
+@SuppressWarnings("unchecked")
+public class Array <T>{
+    private T[] array;
+    private int length = 0;
+
+    private int capacity = 1;
+
+    public Array(){
+        array = (T[]) new Object[capacity];
+    }
+
+    public int size(){
+        return this.length;
+    }
+
+    public boolean isEmpty(){
+        return size() == 0;
+    }
+
+    public void add(final T element){
+        if(length + 1 >= capacity){
+            increasesArray();
+        }
+
+        array[length] = element;
+        length++;
+    }
+
+    public T get(final int index){
+        if(index < 0 && index > length){
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        return array[index];
+    }
+
+    public T removeAt(final int index){
+        if(index < 0 && index > length){
+            throw new ArrayIndexOutOfBoundsException();
+        }
+
+        capacity = capacity - 1;
+        length = length - 1;
+        T element = array[index];
+
+        T[] arrayAux = (T[]) new Object[capacity];
+
+        for(int i =0, j = 0; j<length; i++, j++){
+            if (i == index){
+                j++;
+            }
+            arrayAux[i] = array[j];
+        }
+
+        array = arrayAux;
+        return element;
+    }
+
+    public int indexOf(final T element){
+        for (int i = 0; i < length; i++){
+            if(array[i] == element){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public T remove(final T element){
+        int index = indexOf(element);
+        return removeAt(index);
+    }
+
+    public void set(final T element, final int index){
+        array[index] = element;
+    }
+
+    public void clear(){
+        array = (T[]) new Object[capacity];
+        length = 0;
+        capacity = 1;
+    }
+
+    public boolean contains(T element){
+        return indexOf(element) > -1;
+    }
+
+    private void increasesArray(){
+        capacity = capacity + 1;
+        T[] arrayAux = (T[]) new Object[capacity];
+        for (int i = 0; i < length; i++){
+            arrayAux[i] = array[i];
+        }
+
+        array = arrayAux;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(array);
+    }
 }
--- src/main/java/com/data/structure/linear/array/Main.java
+++ src/main/java/com/data/structure/linear/array/Main.java
@@ -1,7 +1,22 @@
-package com.data.structure;
+package com.data.structure.linear.array;
 
 public class Main {
     public static void main(String[] args) {
-
+        Array<Integer> array = new Array<>();
+        array.add(1);
+        array.add(2);
+        array.add(3);
+        System.out.println(array);
+        System.out.println(array.size());
+        array.remove(3);
+        System.out.println(array);
+        array.add(4);
+        System.out.println(array);
+        System.out.println(array.size());
+        System.out.println(array.get(0));
+        array.clear();
+        array.add(1);
+        System.out.println(array);
+        System.out.println(array.size());
     }
 }
